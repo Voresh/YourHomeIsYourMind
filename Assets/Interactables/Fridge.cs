db51ca7bc04512b35e578d9618acb6a3b77b6d32
Vladimir <voresh@inbox.ru>
2019-01-27 14:40:52 +0300

    fridge opened, hus fix

--- Assets/Interactables/Fridge.cs
+++ Assets/Interactables/Fridge.cs
@@ -6,10 +6,18 @@ public class Fridge: Interactable
 {
 	[SerializeField] 
 	private Transform _dropPosition;
+	[SerializeField] 
+	private SpriteRenderer _spriteRendereer;
+	[SerializeField] 
+	private Sprite _opened;
 	
 	public override List<Tuple<Action, string>> Actions => new List<Tuple<Action, string>>
 	{
-		new Tuple<Action, string>(() => { PillsCreator.CreateAD(_dropPosition); }, "open"),
+		new Tuple<Action, string>(() =>
+		{
+			PillsCreator.CreateAD(_dropPosition);
+			_spriteRendereer.sprite = _opened;
+		}, "open"),
 		new Tuple<Action, string>(null, "exit"),
 	};
 }
